ddbe3388b70f92e5fb031b3d4117e8db35721fcc
maheshwari1705 <maheshwarbhalshankar1705@gmail.com>
2024-04-20 18:48:07 +0530

    jenkins test

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+        System.out.println("We are trying Docker"); 
+        System.out.println("From CDAC ACTS DAC"); 
+        System.out.println("Does this appear in the output???"); 
+        System.out.println("Will we see this???"); 
+        
+
+    }
+}
